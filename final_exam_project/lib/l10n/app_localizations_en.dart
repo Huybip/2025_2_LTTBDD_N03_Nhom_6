689f68c34a7adf8bfd4a897933f8719a8fbaf0f4
Huybip <155954088+Huybip@users.noreply.github.com>
2026-03-01 15:52:30 +0000

    localization_en.dart

---
+++ final_exam_project/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,67 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Music App';
+
+  @override
+  String get tabHome => 'Home';
+
+  @override
+  String get tabDiscovery => 'Discovery';
+
+  @override
+  String get tabAccount => 'Account';
+
+  @override
+  String get tabSettings => 'Settings';
+
+  @override
+  String get loadingSongs => 'Loading songs…';
+
+  @override
+  String get settingsTitle => 'Settings';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get languageEnglish => 'English';
+
+  @override
+  String get languageVietnamese => 'Vietnamese';
+
+  @override
+  String get groupInfo => 'Group info';
+
+  @override
+  String get groupInfoTitle => 'About the team';
+
+  @override
+  String get discoveryTitle => 'Discovery';
+
+  @override
+  String get accountTitle => 'Account';
+
+  @override
+  String get notImplementedYet => 'Not implemented yet';
+
+  @override
+  String get appearance => 'Appearance';
+
+  @override
+  String get themeSystem => 'Use device theme';
+
+  @override
+  String get themeLight => 'Light theme';
+
+  @override
+  String get themeDark => 'Dark theme';
+}
